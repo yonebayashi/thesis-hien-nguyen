4487ac33eff108b992e97abf1cf56feaae0c0118
Hien N <nguyehi@reed.edu>
2019-11-30 17:59:24 -0800

    Update prepare-iwslt15.sh

--- prepare-iwslt15.sh
+++ prepare-iwslt15.sh
@@ -1,113 +1,33 @@
-#!/usr/bin/env bash 
+#!/bin/sh
+# Copied from https://github.com/tensorflow/nmt/blob/master/nmt/scripts/download_iwslt15.sh
+#
+# Download small-scale IWSLT15 Vietnames to English translation data for NMT
+# model training.
+#
+# Usage:
+#   ./download_iwslt15.sh path-to-output-dir
+#
+# If output directory is not specified, "./iwslt15" will be used as the default
+# output directory.
 
-echo 'Cloning Moses github repository (for tokenization scripts)...'
-git clone https://github.com/moses-smt/mosesdecoder.git
+OUT_DIR="${1:-iwslt15}"
+SITE_PREFIX="https://nlp.stanford.edu/projects/nmt/data"
 
-echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
-git clone https://github.com/rsennrich/subword-nmt.git
+mkdir -v -p $OUT_DIR
 
-SCRIPTS=mosesdecoder/scripts
-TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
-LC=$SCRIPTS/tokenizer/lowercase.perl
-CLEAN=$SCRIPTS/training/clean-corpus-n.perl
-BPEROOT=subword-nmt/subword_nmt
-BPE_TOKENS=8000
+# Download iwslt15 small dataset from standford website.
+echo "Download training dataset train.en and train.vi."
+curl -o "$OUT_DIR/train.en" "$SITE_PREFIX/iwslt15.en-vi/train.en"
+curl -o "$OUT_DIR/train.vi" "$SITE_PREFIX/iwslt15.en-vi/train.vi"
 
-URL="https://wit3.fbk.eu/archive/2015-01/texts/en/vi/en-vi.tgz"
-GZ=en-vi.tgz
+echo "Download dev dataset tst2012.en and tst2012.vi."
+curl -o "$OUT_DIR/tst2012.en" "$SITE_PREFIX/iwslt15.en-vi/tst2012.en"
+curl -o "$OUT_DIR/tst2012.vi" "$SITE_PREFIX/iwslt15.en-vi/tst2012.vi"
 
-if [ ! -d "$SCRIPTS" ]; then
-    echo "Please set SCRIPTS variable correctly to point to Moses scripts."
-    exit
-fi
+echo "Download test dataset tst2013.en and tst2013.vi."
+curl -o "$OUT_DIR/tst2013.en" "$SITE_PREFIX/iwslt15.en-vi/tst2013.en"
+curl -o "$OUT_DIR/tst2013.vi" "$SITE_PREFIX/iwslt15.en-vi/tst2013.vi"
 
-src=en
-tgt=vi
-lang=en-vi
-prep=iwslt15.tokenized.en-vi
-tmp=$prep/tmp
-orig=orig
-
-mkdir -p $orig $tmp $prep
-
-echo "Downloading data from ${URL}..."
-cd $orig
-wget "$URL"
-
-if [ -f $GZ ]; then
-    echo "Data successfully downloaded."
-else
-    echo "Data not successfully downloaded."
-    exit
-fi
-
-tar zxvf $GZ
-cd ..
-
-echo "pre-processing train data..."
-for l in $src $tgt; do
-    f=train.tags.$lang.$l
-    tok=train.tags.$lang.tok.$l
-
-    cat $orig/$lang/$f | \
-    grep -v '<url>' | \
-    grep -v '<talkid>' | \
-    grep -v '<keywords>' | \
-    sed -e 's/<title>//g' | \
-    sed -e 's/<\/title>//g' | \
-    sed -e 's/<description>//g' | \
-    sed -e 's/<\/description>//g' | \
-    perl $TOKENIZER -threads 8 -l $l > $tmp/$tok
-    echo ""
-done
-
-perl $CLEAN -ratio 1.5 $tmp/train.tags.$lang.tok $src $tgt $tmp/train.tags.$lang.clean 1 175
-for l in $src $tgt; do
-    perl $LC < $tmp/train.tags.$lang.clean.$l > $tmp/train.tags.$lang.$l
-done
-
-echo "pre-processing valid/test data..."
-for l in $src $tgt; do
-    for o in `ls $orig/$lang/IWSLT15.TED*.$l.xml`; do
-    fname=${o##*/}
-    f=$tmp/${fname%.*}
-    echo $o $f
-    grep '<seg id' $o | \
-        sed -e 's/<seg id="[0-9]*">\s*//g' | \
-        sed -e 's/\s*<\/seg>\s*//g' | \
-        sed -e "s/\’/\'/g" | \
-    perl $TOKENIZER -threads 8 -l $l | \
-    perl $LC > $f
-    echo ""
-    done
-done
-
-echo "creating train, valid, test..."
-for l in $src $tgt; do
-    awk '{if (NR%23 == 0)  print $0; }' $tmp/train.tags.en-vi.$l > $tmp/valid.$l
-    awk '{if (NR%23 != 0)  print $0; }' $tmp/train.tags.en-vi.$l > $tmp/train.$l
-
-    cat $tmp/IWSLT15.TED.dev2010.en-vi.$l \
-        $tmp/IWSLT15.TED.tst2010.en-vi.$l \
-        $tmp/IWSLT15.TED.tst2011.en-vi.$l \
-        $tmp/IWSLT15.TED.tst2012.en-vi.$l \
-        $tmp/IWSLT15.TED.tst2013.en-vi.$l \
-        > $tmp/test.$l
-done
-
-TRAIN=$tmp/train.vi-en
-BPE_CODE=$prep/code
-rm -f $TRAIN
-for l in $src $tgt; do
-    cat $tmp/train.$l >> $TRAIN
-done
-
-echo "learn_bpe.py on ${TRAIN}..."
-python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
-
-for L in $src $tgt; do
-    for f in train.$L valid.$L test.$L; do
-        echo "apply_bpe.py to ${f}..."
-        python $BPEROOT/apply_bpe.py -c $BPE_CODE < $tmp/$f > $prep/$f
-    done
-done
+echo "Download vocab file vocab.en and vocab.vi."
+curl -o "$OUT_DIR/vocab.en" "$SITE_PREFIX/iwslt15.en-vi/vocab.en"
+curl -o "$OUT_DIR/vocab.vi" "$SITE_PREFIX/iwslt15.en-vi/vocab.vi"
