83a9ad7cab5ddfbcc47fb8fd7a81b56251e43a4a
Hien Nguyen <nguyehi@reed.edu>
2020-02-05 22:48:27 -0800

    Add critical files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# thesis-hien-nguyen
-Hien's Thesis
+### Data Processing
+
+To prepare the data for training, start with the following command:
+
+`bash prepare-iwslt15.sh`
+
+ This script downloads the data from [IWSLT'15 English-Vietnamese dataset](https://nlp.stanford.edu/projects/nmt/), cleans and tokenizes it using [Moses tokenizer](https://github.com/moses-smt/mosesdecoder/), and finally applies BPE ([Sennrich et al.](https://github.com/rsennrich/subword-nmt)) on the data.
+
+The number of BPE tokens is set to 4000 by default.
+
+The script applies BPE dropout with `p = 0.1` by default. To disable BPE dropout, remove the optional argument `--dropout 0.1` in the following line:
+
+`python $BPEROOT/apply_bpe.py -c $BPE_CODE --dropout 0.1 < $tmp/train.$L > $tmp/bpe.train.$L`
+
+
+To binarize the data, run:
+
+```
+TEXT=iwslt15.tokenized.en-vi
+
+fairseq-preprocess   --source-lang en --target-lang vi --trainpref $TEXT/train --validpref $TEXT/valid  --testpref $TEXT/test --destdir data-bin/iwslt15_en_vi_bpe4k --workers 20
+```
+
+### Training
+
+To start the training, run:
+
+```
+CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/iwslt15_en_vi_bpe4k/ --lr 0.0001 --optimizer adam --clip-norm 0.0 --dropout 0.2 --max-tokens 4000 --batch-size 32 --arch transformer --update-freq 16 --save-dir checkpoints --no-epoch-checkpoints
+```
+
+All model checkpoints are saved in `./checkpoints`. Since `--no-epoch-checkpoints` is set, only the last checkpoint and the best checkpoint will be saved.
+
+### Translation
+
+To generate Eng>Vie translations, run:
+
+`fairseq-generate data-bin/iwslt15_en_vi_bpe4k/  --source-lang en --target-lang vi --path checkpoints/checkpoint_best.pt --batch-size 8 --beam 4 --remove-bpe`
+
+Translations are generated from the best model checkpoint, using beam search with beam width 4. `remove-bpe` is set to remove BPE delimiters for easier reading.
--- prepare-iwslt15.sh
+++ prepare-iwslt15.sh
@@ -1,33 +1,109 @@
-#!/bin/sh
-# Copied from https://github.com/tensorflow/nmt/blob/master/nmt/scripts/download_iwslt15.sh
-#
-# Download small-scale IWSLT15 Vietnames to English translation data for NMT
-# model training.
-#
-# Usage:
-#   ./download_iwslt15.sh path-to-output-dir
-#
-# If output directory is not specified, "./iwslt15" will be used as the default
-# output directory.
+#!/usr/bin/env bash
 
-OUT_DIR="${1:-iwslt15}"
-SITE_PREFIX="https://nlp.stanford.edu/projects/nmt/data"
+if [ ! -d "mosesdecoder" ]; then
+  echo 'Cloning Moses github repository (for tokenization scripts)...'
+  git clone https://github.com/moses-smt/mosesdecoder.git
+fi
 
-mkdir -v -p $OUT_DIR
+if [ ! -d "subword-nmt" ]; then
+  echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+  git clone https://github.com/rsennrich/subword-nmt.git
+fi
 
-# Download iwslt15 small dataset from standford website.
+SCRIPTS=mosesdecoder/scripts
+TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
+LC=$SCRIPTS/tokenizer/lowercase.perl
+CLEAN=$SCRIPTS/training/clean-corpus-n.perl
+BPEROOT=subword-nmt/subword_nmt
+BPE_TOKENS=4000
+
+SITE_PREFIX="https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi"
+
+if [ ! -d "$SCRIPTS" ]; then
+    echo "Please set SCRIPTS variable correctly to point to Moses scripts."
+    exit
+fi
+
+src=en
+tgt=vi
+lang=en-vi
+prep=iwslt15.tokenized.en-vi
+tmp=$prep/tmp
+orig=orig
+
+mkdir -p $orig $tmp $prep
+
+cd $orig
+
 echo "Download training dataset train.en and train.vi."
-curl -o "$OUT_DIR/train.en" "$SITE_PREFIX/iwslt15.en-vi/train.en"
-curl -o "$OUT_DIR/train.vi" "$SITE_PREFIX/iwslt15.en-vi/train.vi"
+curl -o "train.en" "$SITE_PREFIX/train.en"
+curl -o "train.vi" "$SITE_PREFIX/train.vi"
 
 echo "Download dev dataset tst2012.en and tst2012.vi."
-curl -o "$OUT_DIR/tst2012.en" "$SITE_PREFIX/iwslt15.en-vi/tst2012.en"
-curl -o "$OUT_DIR/tst2012.vi" "$SITE_PREFIX/iwslt15.en-vi/tst2012.vi"
+curl -o "valid.en" "$SITE_PREFIX/tst2012.en"
+curl -o "valid.vi" "$SITE_PREFIX/tst2012.vi"
 
 echo "Download test dataset tst2013.en and tst2013.vi."
-curl -o "$OUT_DIR/tst2013.en" "$SITE_PREFIX/iwslt15.en-vi/tst2013.en"
-curl -o "$OUT_DIR/tst2013.vi" "$SITE_PREFIX/iwslt15.en-vi/tst2013.vi"
+curl -o "test.en" "$SITE_PREFIX/tst2013.en"
+curl -o "test.vi" "$SITE_PREFIX/tst2013.vi"
 
 echo "Download vocab file vocab.en and vocab.vi."
-curl -o "$OUT_DIR/vocab.en" "$SITE_PREFIX/iwslt15.en-vi/vocab.en"
-curl -o "$OUT_DIR/vocab.vi" "$SITE_PREFIX/iwslt15.en-vi/vocab.vi"
+curl -o "vocab.en" "$SITE_PREFIX/vocab.en"
+curl -o "$vocab.vi" "$SITE_PREFIX/vocab.vi"
+
+cd ..
+
+echo "pre-processing train data..."
+for l in $src $tgt; do
+    f=train.$l
+    tok=train.tok.$l
+
+    cat $orig/$f | \
+    perl $TOKENIZER -threads 8 -l $l > $tmp/$tok
+    echo ""
+done
+
+perl $CLEAN -ratio 1.5 $tmp/train.tok $src $tgt $tmp/train.clean 1 175
+for l in $src $tgt; do
+    perl $LC < $tmp/train.clean.$l > $tmp/train.$l
+done
+
+echo "pre-processing valid, test data..."
+for l in $src $tgt; do
+    for f in valid.$l test.$l; do
+      cat $orig/$f | \
+      perl $TOKENIZER -threads 8 -l $l | \
+      perl $LC > $tmp/$f
+      echo ""
+    done
+done
+
+
+TRAIN=$tmp/train.$lang
+BPE_CODE=$prep/code
+rm -f $TRAIN
+for l in $src $tgt; do
+    cat $tmp/train.$l >> $TRAIN
+done
+
+echo "learn_bpe.py on ${TRAIN}..."
+python $BPEROOT/learn_bpe.py -s $BPE_TOKENS < $TRAIN > $BPE_CODE
+
+for L in $src $tgt; do
+    echo "apply_bpe.py to train.$L..."
+    python $BPEROOT/apply_bpe.py -c $BPE_CODE --dropout 0.1 < $tmp/train.$L > $tmp/bpe.train.$L
+done
+
+for L in $src $tgt; do
+    for f in valid.$L test.$L; do
+        echo "apply_bpe.py to ${f}..."
+        python $BPEROOT/apply_bpe.py -c $BPE_CODE < $tmp/$f > $tmp/bpe.$f
+    done
+done
+
+perl $CLEAN -ratio 1.5 $tmp/bpe.train $src $tgt $prep/train 1 250
+perl $CLEAN -ratio 1.5 $tmp/bpe.valid $src $tgt $prep/valid 1 250
+
+for L in $src $tgt; do
+    cp $tmp/bpe.test.$L $prep/test.$L
+done
